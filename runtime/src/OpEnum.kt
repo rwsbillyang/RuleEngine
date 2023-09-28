2f3700feaa7188cdc046752bcda1eee8bdd83a7c
BillYang <rwsbillyang@qq.com>
2023-09-28 15:39:14 +0800

    optimize rrt library

--- runtime/src/OpEnum.kt
+++ runtime/src/OpEnum.kt
@@ -18,30 +18,30 @@
 
 package com.github.rwsbillyang.rule.runtime
 
+interface IEnumOp
 
+val otherCfg = OperandConfig("值", "与另一个值other比较")
 /**
  * 基本操作符
  * */
-enum class EnumOp(val label: String,
-                  val other: Boolean = false,
-                  val start: Boolean = false,
-                  val end: Boolean = false,
-                  val set: Boolean = false,
-                  val e: Boolean = false,
-                  val num: Boolean = false,
-                  val remark: String? = null) {
-    eq("等于", true),
-    ne("不等于", true),
-    gt("大于", true),
-    gte("大于或等于", true),
-    lt("小于", true),
-    lte("小于或等于", true),
+enum class EnumBasicOp(val label: String, val operandMap: Map<String, OperandConfig>):IEnumOp  {
+    eq("等于", mapOf("other" to otherCfg)),
+    ne("不等于", mapOf("other" to otherCfg)),
+    gt("大于", mapOf("other" to otherCfg)),
+    gte("大于或等于", mapOf("other" to otherCfg)),
+    lt("小于", mapOf("other" to otherCfg)),
+    lte("小于或等于", mapOf("other" to otherCfg)),
 
-    between("区间范围",false, true, true),
-    notBetween("不在区间范围",false, true, true),
-
-    `in`("存在于", set = true, remark = "单个元素存在于集合中: 前者存在于后者中"),//单个元素存在于集合中
-    nin("不存在于",  set = true, remark = "单个元素不存在于集合中：前者不存在于后者中"),//单个元素不存在于集合中
+    between("区间范围", mapOf(
+        "start" to OperandConfig("起始值", "范围比较[start, end]"),
+        "end" to OperandConfig("终止值", "范围比较[start, end]")
+    )),
+    notBetween("不在区间范围",mapOf(
+        "start" to OperandConfig("起始值", "范围比较[start, end]"),
+        "end" to OperandConfig("终止值", "范围比较[start, end]")
+    )),
+    `in`("存在于", mapOf("set" to OperandConfig("集合", "单个元素 '存在于' 集合set中", true))),//单个元素存在于集合中
+    nin("不存在于",  mapOf("set" to OperandConfig("集合", "单个元素 '不存在' 于集合set中", true))),//单个元素不存在于集合中
 
     //regexp("正则")
 }
@@ -49,30 +49,23 @@ enum class EnumOp(val label: String,
 /**
  * 集合容器操作符
  * */
-enum class EnumCollectionOp (val label: String,
-                             val other: Boolean = false,
-                             val start: Boolean = false,
-                             val end: Boolean = false,
-                             val set: Boolean = false,
-                             val e: Boolean = false,
-                             val num: Boolean = false,
-                             val remark: String? = null){
-    onlyContains("只包含（等于）", true, remark = "集合所包含元素相同"),
-    contains("包含某元素", e = true, remark ="值集合包含某个元素：前者包含后者"),
-    notContains("不包含某元素", e = true,  remark ="值集合不包含某个元素：前者不包含后者"),
-    containsAll("包含全部", true, remark ="值集合包含所有元素：前者包含所有后者，即后者是前者子集"),
-    anyIn("任意一个存在于", true,remark ="任意元素存在于集合中，前者任意一个元素存在于后者中，即交集非空"),//即numberOfIn > 0
-    numberIn("有几个存在于",  true,num = true, remark ="两个集合交集中的元素是几个"),
-    gteNumberIn("至少几个存在于",  num = true, remark ="两个集合交集中的元素不小于几"),//集合v0存在于集合v1中的元素数量
-    lteNumberIn("至多几个存在于", true, num = true,remark ="两个集合交集中的元素不大于几"),//集合v0存在于集合v1中的元素数量
-    allIn("都存在于",  true, remark ="元素全部元素存在于集合中: 前者都存在于后者中，即前者是后者子集"),//元素全部元素存在于集合中，即是子集 即numberOfIn == sizeOf(collection1)
-    allNotIn("都不存在于", true, remark ="元素全部元素都不存在于集合中，前者任何一个元素都不出现于后者中，二者无交集"),
+enum class EnumCollectionOp (val label: String, val operandMap: Map<String, OperandConfig>):IEnumOp {
+    onlyContains("只包含（等于）", mapOf("other" to OperandConfig("集合", "集合所包含元素相同，即等于", true)) ),
+    contains("包含某元素", mapOf("e" to OperandConfig("元素e", "是否包含某个元素e"))),
+    notContains("不包含某元素", mapOf("e" to OperandConfig("元素e", "是否不包含某个元素e"))),
+    containsAll("包含全部", mapOf("other" to OperandConfig("集合", "集合包含所有other的元素，即other是子集", true))),
+    anyIn("任意一个存在于", mapOf("other" to OperandConfig("集合", "任意元素存在于集合other中，任意一个元素存在于other中，即交集非空", true))),//即numberOfIn > 0
+    numberIn("有几个存在于",  mapOf("other" to OperandConfig("集合", "有num个元素存在于集合other中", true), "num" to OperandConfig("num", "两个集合交集中的元素是num个",false, true, IType.Type_Int))),
+    gteNumberIn("至少几个存在于", mapOf("other" to OperandConfig("集合", "有num个元素存在于集合other中", true), "num" to OperandConfig("num", "两个集合交集中的元素不小于num个",false, true,  IType.Type_Int))),//集合v0存在于集合v1中的元素数量
+    lteNumberIn("至多几个存在于", mapOf("other" to OperandConfig("集合", "有num个元素存在于集合other中", true), "num" to OperandConfig("num", "两个集合交集中的元素不大于num个",false, true,  IType.Type_Int))),//集合v0存在于集合v1中的元素数量
+    allIn("都存在于",  mapOf("other" to OperandConfig("集合", "元素全部元素存在于集合other中, 是other的子集", true))),//元素全部元素存在于集合中，即是子集 即numberOfIn == sizeOf(collection1)
+    allNotIn("都不存在于",  mapOf("other" to OperandConfig("集合", "元素全部元素都不存在于集合other中，二者无交集", true))),
 }
 
 /**
  * 表达式的逻辑运算符
  * */
-enum class EnumLogicalOp(val label: String, val remark: String? = null){
+enum class EnumLogicalOp(val label: String, val remark: String? = null):IEnumOp {
     or("或者", "逻辑或(OR)"),
     and("并且", "逻辑且(AND)"),
     none("均非", "逻辑都不是(All NOT)"),
---
+++ runtime/src/Oprand.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright © 2023 rwsbillyang@qq.com
+ *
+ * Written by rwsbillyang@qq.com at Beijing Time: 2023-09-26 10:45
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.rwsbillyang.rule.runtime
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+
+/**
+ * 一个逻辑表达式构成因素：表达式类型（变量类型决定）、变量（mapKey或记录库中添加的变量记录）、操作码、若干操作数构成
+ * 不同的操作码会有不同的操作数，OperandConfig正是操作数的配置
+ * 系统内置操作码预先生成，不可修改
+ *
+ * 放入runtime库，用于初始化各种OpEnum，然后插入数据库Opcode表中
+ *
+ * @param label 创建表达式或规则时，输入时操作数时的提示标签
+ * @param tooltip 创建表达式或规则时，输入时操作数时的提示帮助提示
+ * @param multiple 用于创建表达式或规则时，选择输入框过滤常量是否多选 当typeCode是基本类型时，操作数也可能是其对应的容器类型，
+ * @param required 操作数是否必须填写
+ * @param typeCode 操作数数据类型，null表示与变量类型一致；用于创建表达式或规则时，选择输入框过滤常量或变量
+ * @param contantIds 操作数值域，用于创建表达式或规则时，选择输入框过滤常量 UI中创建自定义操作码时需要指定
+ * @param selectOptions 操作数值域，不是从远程加载常量，而是指定的选择范围内选
+ * @param defaultSelect 操作数值域select默认值
+ * @param defaultOperandValueType 前端OperandMeta.valueType: "Param" | "Constant" | "JsonValue"
+ * @param enable 一般总是激活状态
+ *
+ * */
+@Serializable
+class OperandConfig(
+    val label: String,
+    val tooltip: String? = null,
+    val multiple: Boolean = false, //
+    val required: Boolean = true, //操作数是否必须填写
+    val typeCode: String? = null,//操作数数据类型，null表示操作数与与变量类型一致
+    val contantIds: List<Int>? = null, //操作数值域，常量id数组[1,2,3]
+    val selectOptions: List<SelectOption>? = null,//操作数值域，不是从远程加载常量，而是指定的选择范围内选
+    val defaultSelect: String? = null,
+    val defaultOperandValueType: String? = null, //"Param" | "Constant" | "JsonValue"
+    val enable: Boolean = true
+)
+@Serializable
+class SelectOption(val label: String, val value: String)
+
+@Serializable
+enum class OperandValueType{ Param, Constant,JsonValue } //前端OperandMeta.valueType
+/**
+ * 操作数
+ * @param name 操作数名称
+ * @param typeCode 操作数类型code，null表示与变量类型一致
+ * @param
+ * */
+@Serializable
+class Operand(
+    val valueType: OperandValueType,
+    val key: String? = null, //key: other, start, end, set, e, number, if valueType == ValueType.Param then use key to pick a value
+    @Contextual val value: Any? = null //else use value directly
+){
+    fun real(dataProvider: (key: String, keyExtra:String?) -> Any?, keyExtra: String? = null) = if(valueType == OperandValueType.Param && key != null){
+        dataProvider(key, keyExtra)
+    }else{
+        value
+    }
+}
+
